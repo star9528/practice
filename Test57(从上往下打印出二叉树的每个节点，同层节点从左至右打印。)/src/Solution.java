92cdec8599c9215d877bbb553c445cccc17dfdc7
飞 <1752840757@qq.com>
2020-07-25 23:28:57 +0800

    从上往下打印出二叉树的每个节点，同层节点从左至右打印。

---
+++ Test57(从上往下打印出二叉树的每个节点，同层节点从左至右打印。)/src/Solution.java
@@ -0,0 +1,37 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class TreeNode {
+    int val = 0;
+    TreeNode left = null;
+    TreeNode right = null;
+
+    public TreeNode(int val) {
+        this.val = val;
+
+    }
+}
+
+public class Solution {
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        ArrayList<Integer> list = new ArrayList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            list.add(node.val);
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
+            }
+        }
+        return list;
+    }
+}
